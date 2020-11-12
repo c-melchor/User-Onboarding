8fb277df5d9cd2424f9275696afccec93557d5c1
c_melchor <cah.melchor@gmail.com>
2020-11-12 12:49:57 -0800

    email, password, checkbox, and submit tests done

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -2,11 +2,50 @@ describe("Form App", () => {
   beforeEach(() => {
     cy.visit("localhost:3000");
   });
+  const submitButton = () => cy.get('button[type="submit"]');
+  const termsBoxCheck = () => cy.get('input[name="terms"]');
+  const passwordInput = () => cy.get('input[name="password"]');
+  const nameInput = () => cy.get('input[name="name"]');
+  const emailInput = () => cy.get('input[name="email"]');
 
+  //name input test
   it("name check", () => {
-    const nameInput = () => cy.get('input[name="name"]');
     nameInput()
       .type("Christina Melchor")
       .should("have.value", "Christina Melchor");
+  });
+
+  //email input test
+  it("email input check", () => {
+    emailInput()
+      .type("hi@me.com")
+      .should("have.value", "hi@me.com");
+  });
+
+  //password input test
+  it("password input check", () => {
+    passwordInput()
+      .type("password!")
+      .should("have.value", "password!");
+  });
+
+  //terms checkbox test
+  it("terms checkbox able to be checked", () => {
+    termsBoxCheck()
+      .click()
+      .should("be.checked");
+  });
+
+  //submit button check
+  it("submit button check", () => {
+    nameInput().type("christina");
+    emailInput().type("email@me.com");
+    passwordInput().type("password");
+    submitButton().click();
+    cy.contains("christina").should("exist");
+    cy.contains("email@me.com").should("exist");
+    cy.contains("password").should("exist");
+    cy.contains("on");
+    // cy.contains("").should("exist");
   });
 });
